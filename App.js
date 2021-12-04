2153530e4c98e7f85beef4c5ab29b70a8c364113
{2018121152@yonsei.ac.kr} <2018121152@yonsei.ac.kr>
2021-12-04 21:36:08 +0900

    added  title, button, random number extractor

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
+  const[lotto, setLotto] = useState([]);
+
+  function makeLotto(){
+    let i = 0;
+    let lnumber = [];
+    let ran_num;
+    for (i = 0; i < 6; i++)
+    {
+      ran_num = Math.floor(Math.random()* 45 + 1);
+      if(lnumber.includes(ran_num))
+       i--; 
+      else
+      lnumber.push(ran_num);      
+    }
+    setLotto(lnumber);
+  }
+  useEffect(()=>{
+    makeLotto();
+  },[]);
+
+  return(
     <View style={styles.container}>
-      <Text>Hello Hyunseok and Dongwoo!</Text>
+      <Text style={{fontSize : 25, marginBottom: 150}}>Welcome to Lotto Number</Text>
+      <Button title = "무료 추천 번호 받기" onPress={()=>makeLotto()}/>
+      <Text style={{fontSize : 25, marginBottom: 150}}> {lotto.toLocaleString()}</Text>
       <StatusBar style="auto" />
     </View>
   );
